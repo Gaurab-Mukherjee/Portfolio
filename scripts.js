18f00b9e39e96627f9233bc237ae2f0a81e32733
Gaurab Mukherjee <gaurabmukherjee20@gmail.com>
2024-08-26 17:28:42 +0530

    Your commit message

--- scripts.js
+++ scripts.js
@@ -18,3 +18,40 @@ window.addEventListener('scroll', function() {
 document.getElementById('scroll-to-top').addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Computer Science Engineer",
+      "Full Stack Developer",
+      "Lifelong Learner",
+      "Android Developer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+
+
+//   // owl carousel script
+//   $(".carousel").owlCarousel({
+//     margin: 20,
+//     loop: true,
+//     autoplayTimeOut: 2000,
+//     autoplayHoverPause: true,
+//     responsive: {
+//       0: {
+//         items: 1,
+//         nav: false
+//       },
+//       600: {
+//         items: 2,
+//         nav: false
+//       },
+//       1000: {
+//         items: 3,
+//         nav: false
+//       }
+//     }
+//   });
